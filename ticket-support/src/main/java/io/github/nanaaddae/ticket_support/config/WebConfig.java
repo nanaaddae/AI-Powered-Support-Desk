61e8ed298b4677dc09d72e7c0d8f7e4fe8eab3dd
nanaaddae <nana_addae@yahoo.com>
2026-06-24 02:02:42 -0400

    adjusted CORS for Vercel

--- ticket-support/src/main/java/io/github/nanaaddae/ticket_support/config/WebConfig.java
+++ ticket-support/src/main/java/io/github/nanaaddae/ticket_support/config/WebConfig.java
@@ -10,10 +10,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173",
+                    "https://ai-powered-support-desk.vercel.app/"
+
+                )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+                
     }
 }
